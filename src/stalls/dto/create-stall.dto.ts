2df1d35fc0fc93226167c1b10ebffa179abf0f30
Muhammad Waqas Siddique <siddique02mwaqas@gmail.com>
2025-07-07 00:15:25 +0500

    added api property() tags in dtos

--- src/stalls/dto/create-stall.dto.ts
+++ src/stalls/dto/create-stall.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsInt, IsNotEmpty, IsNumber, IsOptional, Max, Min } from "class-validator";
 
 export class CreateStallDto {
+  @ApiProperty()
   @IsInt()
   @IsNotEmpty()
   cafeteria_id: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsNumber({ maxDecimalPlaces: 2 })
   @Min(0)
