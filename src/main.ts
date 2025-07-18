5c9454b4fee37987f8a1e37eba7c5100c10ac48f
Muhammad Waqas Siddique <siddique02mwaqas@gmail.com>
2025-07-19 02:27:53 +0500

    added interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ResponseInterceptor } from './interceptors/response/response.interceptor';
+import { AllExceptionsFilter } from './interceptors/exception/exception.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,8 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   
   await app.listen(process.env.PORT ?? 3000).then(()=>{
     console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
